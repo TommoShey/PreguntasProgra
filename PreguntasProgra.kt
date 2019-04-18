6887d9af6ee322ca8102aef2d1f95da7b2feceeb
TommoShey <49286084+TommoShey@users.noreply.github.com>
2019-04-17 20:31:41 -0600

    Add files via upload

---
+++ PreguntasProgra.kt
@@ -0,0 +1,39 @@
+/**Sheyla Marisol Guidos Berrios*/
+
+/**¿Qué es el control de versiones?*/
+/**El control de versiones es un sistema que registra los cambios realizados sobre un archivo o conjunto de archivos a lo largo del tiempo, */
+/**de modo que puedas recuperar versiones específicas más adelante.*/
+/**A pesar de que los ejemplos de este libro muestran código fuente como archivos bajo control de versiones, en realidad cualquier tipo de archivo */
+/**que encuentres en un ordenador puede ponerse bajo control de versiones.*/
+
+/**¿Cuales son las ventajas de utilizar un contr de versiones?*/
+/**Acceso al registro de modificaciones sobre un fichero. Esto nos permite además ver los comentarios asociados */
+/**a cada modificación y la persona que ha realizado dicha modificación*/
+/**Posibilidad de regresar al estado anterior de un fichero o conjunto de ficheros*/
+/**Creación de ramas para gestionar cambios que finalmente se mezclarán con la rama principal*/
+
+/**¿Cuales son los controladores de versiones disponibles para Android Studio?*/
+/**Git y GitHub*/
+
+/**¿Cuales son los controladores de versiones mas conocidos?*/
+/**Existen muchos Sistemas de Control de Versiones siendo algunos de los más conocidos CVS, Git, Subversion, Mercurial y Baazar.*/
+
+/**Investigue:*/
+/**Push: apilar (push), que coloca un objeto en la pila, y su operación inversa, retirar (o desapilar, pop), que retira el último elemento apilado.*/
+
+/**Commit:La operación commit indica la finalización de una transacción satisfactoria.*/
+/**Indica al administrador de transacciones que una unidad de trabajo lógica ha concluido satisfactoriamente, que la base de datos está o debería estar nuevamente en un estado consistente.*/
+/**En definitiva, se refiere a cuando una transacción ha completado su ejecución con éxito, o sea, ingresa en el estado committed.*/
+
+/**Branch (rama): cuando se van a llevar a cabo cambios importantes que romperán la compilación, pruebas, experimentos o intentos de optimización, debe crearse una nueva rama de desarrollo,*/
+/**con la que no molestemos a los compañeros, esto es un branch: una copia del código o la rama de la que deriva. En esta copia haremos nuestros cambios, integraremos los arreglos que puedan haberse ido haciendo en el trunk, y,*/
+/**una vez terminado el desarrollo en la rama, integraremos (o no) los cambios en el trunk. También puede crearse una rama para una versión terminada, hacer mantenimiento de esta versión sobre esta rama, y continuar el desarrollo de la nueva versión en el trunk.*/
+
+/**Delete: es una palabra en inglés que puede traducirse como «eliminar», «suprimir» o «borrar».*/
+
+/**Stack: Una pila (stack en inglés) es una lista ordenada o estructura de datos que permite almacenar y recuperar datos, el modo de acceso a sus elementos es */
+/**de tipo LIFO (del inglés Last In, First Out, «último en entrar, primero en salir») .*/
+/**Esta estructura se aplica en multitud de supuestos en el área de informática debido a su simplicidad y capacidad de dar respuesta a numerosos procesos.*/
+
+/**Repositorio: es un espacio que se utiliza para almacenar distintas cosas. En este sentido, actualmente se suele hacer referencia a las bases de datos digitales */
+/**y a diversos sistemas informáticos como repositorios.*/
